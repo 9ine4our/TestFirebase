e210e945db3bb361be875e77137af7cc28d58406
9ine4our <148853314+9ine4our@users.noreply.github.com>
2024-01-11 12:29:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
-
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyAIeCy_WOb2wdFkJ0aKKNfnjEs5ddMdAHw",
     authDomain: "upload-98f07.firebaseapp.com",
     databaseURL: "https://upload-98f07-default-rtdb.firebaseio.com",
@@ -15,5 +8,3 @@
     appId: "1:916259211600:web:1377d31f58ad4502b18455"
   };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
